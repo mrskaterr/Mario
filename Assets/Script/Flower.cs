0d687b68ce4db4ba106ff31d9b6d17d432aab787
MrSkaterVV <MrSkaterVV@gmail.com>
2020-11-20 09:31:26 +0100

    Mapa

---
+++ Assets/Script/Flower.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flower : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.name == "Player")
+        {
+            //collision.gameObject.transform.localScale = new Vector2(1.1f, 1.5f);
+            gameObject.SetActive(false);
+        }
+    }
+}
